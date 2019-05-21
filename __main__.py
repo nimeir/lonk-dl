291f04409cad6f95acd0d648d1742928e80c9dbb
nimeir <rj8205562@gmail.com>
2019-05-21 19:19:39 +0100

    Return null when header is html

--- __main__.py
+++ __main__.py
@@ -1,25 +1,28 @@
 import praw,requests,re
 
 class Reddit(praw.Reddit):
-    def determine_filename(self, post, url):
+    def determine_filename(self, post, url, request):
         if self.submission(id=post).is_self:
+            return None
+        elif 'text/html' in request.headers['content-type']:
             return None
         else:
             filename = url.split("/")
             filename = filename[-1].replace('?', '')
             return filename
 
-    def extract_url(self):
-        for post in self.subreddit('earthporn').hot(limit=5):
+    def extract_info(self):
+        for post in self.subreddit('linkiscute').hot(limit=5):
             url = post.url
-            content = requests.get(url).content
-            filename = self.determine_filename(post, url, content)
+            request = requests.get(url)
+            filename = self.determine_filename(post, url, request)
             if filename is None:
                 continue
+            content = request.content
             yield url, filename, content
 
 r = Reddit()
-for url, filename, content in r.extract_url():
+for url, filename, content in r.extract_info():
     try:
         with open(filename, "xb") as f:
             f.write(content)
