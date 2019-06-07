36f29bdf1736f68df8070230b1060477950583b9
nimeir <rj8205562@gmail.com>
2019-06-07 02:28:18 +0100

    Major code restructuring

--- lonk_dl.py
+++ lonk_dl.py
@@ -3,45 +3,54 @@ import praw, requests, argparse, os
 
 # Extend the praw.Reddit class
 class Reddit(praw.Reddit):
-    def determine_filename(self, post, url, request):
-        if self.submission(id=post).is_self:
-            return None
-        elif 'text/html' in request.headers['content-type']:
-            return None
-        else:
-            filename = url.split("/")
-            filename = filename[-1].replace('?', '')
-            return filename
+    def determine_filename(self, url):
+        filename = url.split("/")
+        filename = filename[-1].replace('?', '')
+        return filename
+
+    def real_extract(self, post, request, url):
+        url = post.url
+        filename = self.determine_filename(url)
+        content = request.content
+        return url, filename, content
 
     def extract_info(self, subreddit, post_limit, sort, no_nsfw):
-        if '/u/' or 'u/' in subreddit:
-            mobj = getattr(self.redditor(subreddit), sort)(limit=post_limit)
-        else:
-            mobj = getattr(self.subreddit(subreddit), sort)(limit=post_limit)
+        #for redditors
+        if args.redditor:
+            if args.sort == 'saved':
+                mobj = self.redditor(subreddit).saved(limit=post_limit)
+            mobj = getattr(self.redditor(subreddit).submissions, sort)(limit=post_limit)
 
+        #for subreddits
+        mobj = getattr(self.subreddit(subreddit), sort)(limit=post_limit)
+
+        #begin extraction with correct mobj
         for post in mobj:
-            if no_nsfw and self.submission(id=post).over_18:
-                continue
             url = post.url
             request = requests.get(url)
-            filename = self.determine_filename(post, url, request)
-            if filename is None:
+            if self.submission(id=post).is_self:
                 continue
-            content = request.content
-            yield url, filename, content
+            if no_nsfw and self.submission(id=post).over_18:
+                continue
+            if 'text/html' in request.headers['content-type']:
+                continue
+            yield self.real_extract(post, request, url)
+
 
 
 def parse_arguments():
     parser = argparse.ArgumentParser(formatter_class=argparse.ArgumentDefaultsHelpFormatter)
     parser.add_argument('subreddit', nargs='?')
+    parser.add_argument('-i', action='store_true', help='Create praw.ini')
     parser.add_argument('--limit', '-l', type=int, default=1000,
                         help="Set the limit for maximum number of posts that will be requested")
-    parser.add_argument('--sort', '-t', type=str, default='new',
-                        help="Set frontpage sort type. For example: 'hot', 'controversial'")
-    parser.add_argument('--path', '-p', type=str, help="Specify the download directory path")
     parser.add_argument('--no-nsfw', action='store_true',
                         help="Do not download images that are marked nsfw")
-    parser.add_argument('-i', action='store_true', help='Create praw.ini')
+    parser.add_argument('--path', '-p', type=str, help="Specify the download directory path")
+    parser.add_argument('--sort', '-t', type=str, default='new',
+                        help="Set frontpage sort type. For example: 'hot', 'controversial'")
+    parser.add_argument('--redditor', '-r', action='store_true',
+                        help="Extract from redditor instead of subreddit.")
     return parser.parse_args()
 
 
@@ -67,6 +76,7 @@ def main():
         except FileExistsError:
             print("[%s] File already exists.\nTerminating script." % filename)
             break
+
 
 
 if __name__ == '__main__':
